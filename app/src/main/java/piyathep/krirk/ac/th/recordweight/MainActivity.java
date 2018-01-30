dde7f0084085389d145835a2d5df81ee0c7789a4
Piyathep <pmg_1037@hotmail.com>
2018-01-30 11:58:13 +0700

    Finish 30 JAN

--- app/src/main/java/piyathep/krirk/ac/th/recordweight/MainActivity.java
+++ app/src/main/java/piyathep/krirk/ac/th/recordweight/MainActivity.java
@@ -3,11 +3,19 @@ package piyathep.krirk.ac.th.recordweight;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import piyathep.krirk.ac.th.recordweight.utility.MyManager;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MyManager myManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        myManager = new MyManager(MainActivity.this);
+
+    }   // Main Method
+
+}   // Main Class
